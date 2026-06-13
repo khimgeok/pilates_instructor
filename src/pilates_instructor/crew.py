44b12b8211f8c53962301923c0e56c97e0b5d9d6
khimgeok <khimgeok_lite@hottmail.com>
2026-06-13 12:38:46 +0800

    update crew.py to change the task name to align with tasks.yaml

--- src/pilates_instructor/crew.py
+++ src/pilates_instructor/crew.py
@@ -10,55 +10,37 @@ from typing import List
 class PilatesInstructor():
     """PilatesInstructor crew"""
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
-
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
-    @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
 
     @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
+    def pilates_researcher(self) -> Agent:
+        return Agent(config=self.agents_config['pilates_researcher'], verbose=True)
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-    @task
-    def research_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
-        )
+
+    @agent
+    def workout_planner(self) -> Agent:
+        return Agent(config=self.agents_config['workout_planner'], verbose=True)
+
+    @agent
+    def pilates_instructor(self) -> Agent:
+        return Agent(config=self.agents_config['pilates_instructor'], verbose=True)
 
     @task
-    def reporting_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
-        )
+    def research_workout_task(self) -> Task:
+        return Task(config=self.tasks_config['research_workout_task'])
+
+    @task
+    def generate_audio_script_task(self) -> Task:
+        return Task(config=self.tasks_config['generate_audio_script_task'], output_file='script.md')
 
     @crew
     def crew(self) -> Crew:
         """Creates the PilatesInstructor crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
